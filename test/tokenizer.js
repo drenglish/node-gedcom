f979891c06835d9709e8d862876391762ad21684
Michael Dietz <mdd.drenglish@gmail.com>
2017-03-16 17:58:38 -0500

    Reactive tokenizer with Bacon.js

--- test/tokenizer.js
+++ test/tokenizer.js
@@ -1,7 +1,7 @@
 var chai = require('chai'),
 	fs = require('fs'),
 	path = require('path'),
-	Tokenizer = require('../lib/Tokenizer');
+	{tokenStream} = require('../lib/Tokenizer');
 
 chai.should();
 
@@ -10,32 +10,34 @@ var gedtext = fs.readFileSync(path.join(process.cwd(),'ged/family.ged'), {encodi
 	persCount = gedtext.match(/^0.+INDI/gm).length;
 
 describe('A GED file tokenizer', () => {
-	it('should match against all non-blank input lines', (cb) => {
-		let tokenizer = new Tokenizer({parse: false}),
-			total = 0;
+	it('should map all non-blank input lines', (cb) => {
+		let ts = tokenStream( fs.createReadStream(path.join(process.cwd(),'ged/family.ged')) );
+		let total = 0;
 
-		tokenizer.on('data', function(tokenized) {
+		ts.onValue(tok => {
 			total++;
-		})
-
-		tokenizer.on('end', function() {
+			return () => { total = 0; }
+		});
+		ts.onEnd(() => {
 			lineCount.should.equal(total);
 			cb();
+
+			return () => { total = 0; }
 		});
-		tokenizer.tokenize(fs.createReadStream(path.join(process.cwd(),'ged/family.ged'), {encoding: 'utf8'}));
 	});
 	it('should emit proper tokens for every person entry', (cb) => {
-		var tokenizer = new Tokenizer();
-		var indiTokens = 0;
+		let ts = tokenStream( fs.createReadStream(path.join(process.cwd(),'ged/family.ged')) );
+		let total = 0;
 
-		tokenizer.on('data', function(tokenized) {
-			if (tokenized.level === 0 && tokenized.tag === 'INDI') indiTokens++;
+		ts.onValue(tok => {
+			if (tok.level === 0 && tok.tag === 'INDI') total++;
+			return () => { total = 0; }
 		});
-
-		tokenizer.on('end', function() {
-			indiTokens.should.equal(persCount);
+		ts.onEnd(() => {
+			persCount.should.equal(total);
 			cb();
+
+			return () => { total = 0; }
 		});
-		tokenizer.tokenize(fs.createReadStream(path.join(process.cwd(),'ged/family.ged'), {encoding: 'utf8'}));
 	});
 });
